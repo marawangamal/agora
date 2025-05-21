954b08fc7156759cc0f50e13e378e88c5c3c3827
Marawan Gamal <marawan.gamal@live.com>
2025-05-21 18:23:07 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tjdnet",  # or whatever your project is called
+    version="0.1.0",
+    packages=find_packages(include=["jrun*"]),
+    install_requires=["tabulate>=0.9.0"],
+    entry_points={
+        "console_scripts": ["jrun = jrun.main:main"],
+    },
+)
