46ddf206ec19f125a75f5d5836efb97597c58527
Marawan Gamal <marawan.gamal@live.com>
2025-05-21 18:33:09 -0400

    Bugfix: update setuptools project name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tjdnet",  # or whatever your project is called
-    version="0.1.0",
+    name="jrun",  # or whatever your project is called
+    version="1.0.0",
     packages=find_packages(include=["jrun*"]),
     install_requires=["tabulate>=0.9.0", "PyYAML>=6.0"],
     entry_points={
