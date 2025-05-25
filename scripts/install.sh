10a35761c88c22a5f3732fe0ee6a209d00dcedf5
Marawan Gamal <marawan.gamal@live.com>
2025-05-25 14:37:33 -0400

    Refactor: remove prev web dir

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Node.js and npm via nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+# Load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node LTS
+nvm install --lts
+nvm use --lts
+nvm alias default lts/*
+
+echo "Done. Node/npm ready in this session."
+echo "For new terminals, run: source ~/.bashrc && source .venv/bin/activate"
+
+nvm use --lts
